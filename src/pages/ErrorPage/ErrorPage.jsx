707d64d3789cea7d7df1e5ef042d821d1b8aef5d
Abdullah <mda036060@gmail.com>
2026-03-14 10:32:08 +0600

    addad all files with first commit

---
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div className='flex justify-center items-center pt-40'>
+            <h1>Page Not Founded</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
